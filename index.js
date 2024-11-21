2fae633e9028087f1dce46a2d33d47b3d5281029
Caleb Kiptoo <vincentcalebkip@gmail.com>
2024-11-21 23:22:50 +0300

    create a card list

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+//for (let age = 30; age < 40; age++) {
+//    console.log(`I'm ${age} years old. Happy birthday to me!`);
+//    debugger;
+//  }
+const message = []
+function writeCards(myArr,event){
+    for(let count=0;count<myArr.length;count++){
+        message.push(`Thank you, ${myArr[count]}, for the wonderful ${event} gift!`)
+    }
+    return message
+
+}
+function countDown(number){
+    while(number>=0){
+        console.log(number)
+        number-=1
+    }
+}
